68097aa6cc48e3be08861f31b86f5e75e48080cb
Tanupriya91 <tp6677@srmist.edu.in>
2026-07-12 12:32:44 +0530

    Created SOCKET.IO test client

--- Backend/sockets/index.js
+++ Backend/sockets/index.js
@@ -1,10 +1,12 @@
 const initializeSocket = (io) => {
-    io.on("connection", (socket) => {
-        console.log(`User connected: ${socket.id}`);
+  io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
 
-        socket.on("disconnected", () => {
-            console.log(`User disconnected: ${socket.id}`);
-        });
+    socket.on("disconnect", (reason) => {
+      console.log(`User disconnected: ${socket.id}`);
+      console.log(`Reason: ${reason}`);
     });
+  });
 };
+
 export default initializeSocket;
