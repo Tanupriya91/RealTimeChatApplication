095f712785e22cfddd929d3904d944de9e708dfc
Tanupriya91 <tp6677@srmist.edu.in>
2026-07-12 17:53:13 +0530

    Implemented socket.IO room joining

--- Backend/sockets/index.js
+++ Backend/sockets/index.js
@@ -1,9 +1,12 @@
 import socketAuthMiddleware from "./authMiddleware.js";
+import registerRoomHandlers from "./handlers/roomHandler.js";
 const initializeSocket = (io) => {
     io.use(socketAuthMiddleware);
   io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
     console.log(`Firebase UID: ${socket.data.user.uid}`);
+
+    registerRoomHandlers(io,socket);
 
     socket.on("disconnect", (reason) => {
       console.log(`User disconnected: ${socket.id}`);
